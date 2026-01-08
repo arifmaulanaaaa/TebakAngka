7c188bf7b8e6786e77d93e95f0910a13e3ee1bcd
arifmaulanaaaa <arif270505@gmail.com>
2026-01-09 01:58:00 +0700

    Menambahkan fitur main lagi.

--- src/Main.java
+++ src/Main.java
@@ -5,72 +5,79 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Random random = new Random();
-        boolean lanjut = true;
+        boolean mainLagi = true;
 
-        int angkaAcak;
-        int jumlahPercobaan = 0;
-        int maxTebakan = 0;
-        int pilihanLevel = 0;
+        while (mainLagi) {
+            boolean lanjut = true;
+            int angkaAcak;
+            int jumlahPercobaan = 0;
+            int maxTebakan = 0;
+            int pilihanLevel = 0;
 
-        angkaAcak = random.nextInt(1, 100);
+            angkaAcak = random.nextInt(1, 100);
 
-        System.out.println("Selamat datang di game tebak angka!");
-        System.out.println("pilih level kesulitan: ");
-        System.out.println("1 EASY (10 percobaan)");
-        System.out.println("2 MEDIUM (5 percobaan)");
-        System.out.println("3 Hard (3 percobaan)");
-        System.out.println("pilihan mu (1/2/3): ");
+            System.out.println("Selamat datang di game tebak angka!");
+            System.out.println("pilih level kesulitan: ");
+            System.out.println("1 EASY (10 percobaan)");
+            System.out.println("2 MEDIUM (5 percobaan)");
+            System.out.println("3 Hard (3 percobaan)");
+            System.out.println("pilihan mu (1/2/3): ");
 
-        if (input.hasNextInt()) {
-            pilihanLevel = input.nextInt();
-        } else {
-            System.out.println("Pilihan tidak valid. Kamu dialihkan ke level Medium!");
-            pilihanLevel = 2;
-            input.next();
-        }
-
-        if (pilihanLevel == 1) {
-            maxTebakan = 10;
-        } else if (pilihanLevel == 2) {
-            maxTebakan = 5;
-        } else if (pilihanLevel == 3) {
-            maxTebakan = 3;
-        } else {
-            System.out.println("Tidak valid! Kamu dialihkan ke level MEDIUM.");
-            maxTebakan = 5;
-        }
-
-        System.out.println("Saya telah memiliki angka dari 1 sampai 100, silahkan tebak!");
-        System.out.println("============================================================");
-
-        while (jumlahPercobaan < maxTebakan && lanjut == true) {
-
-            System.out.println("Masukkan tebakan anda: ");
-            int tebakan = input.nextInt();
-            jumlahPercobaan++;
-
-            if (tebakan < angkaAcak) {
-                System.out.println("Tebakan terlalu rendah! coba lagi.");
-            } else if (tebakan > angkaAcak) {
-                System.out.println("Tebakan terlalu tinggi! coba lagi.");
+            if (input.hasNextInt()) {
+                pilihanLevel = input.nextInt();
             } else {
-                System.out.println("Selamat! anda berhasil menebak dalam " + jumlahPercobaan + " Percobaan.");
-                lanjut = false;
+                System.out.println("Pilihan tidak valid. Kamu dialihkan ke level Medium!");
+                pilihanLevel = 2;
+                input.next();
             }
 
-            if (lanjut && jumlahPercobaan < maxTebakan) {
-                System.out.println("Sisa percobaan: " + (maxTebakan - jumlahPercobaan));
+            if (pilihanLevel == 1) {
+                maxTebakan = 10;
+            } else if (pilihanLevel == 2) {
+                maxTebakan = 5;
+            } else if (pilihanLevel == 3) {
+                maxTebakan = 3;
+            } else {
+                System.out.println("Tidak valid! Kamu dialihkan ke level MEDIUM.");
+                maxTebakan = 5;
             }
 
-        }
+            System.out.println("Saya telah memiliki angka dari 1 sampai 100, silahkan tebak!");
+            System.out.println("============================================================");
 
-        if (lanjut == true) {
-            System.out.println("Anda sudah memenuhi batas menebak!");
-            System.out.println("Angka rahasianya nya adalah " + angkaAcak);
-            System.out.println("Terimakasih sudah bermain.");
-        }
+            while (jumlahPercobaan < maxTebakan && lanjut == true) {
 
+                System.out.println("Masukkan tebakan anda: ");
+                int tebakan = input.nextInt();
+                jumlahPercobaan++;
+
+                if (tebakan < angkaAcak) {
+                    System.out.println("Tebakan terlalu rendah! coba lagi.");
+                } else if (tebakan > angkaAcak) {
+                    System.out.println("Tebakan terlalu tinggi! coba lagi.");
+                } else {
+                    System.out.println("Selamat! anda berhasil menebak dalam " + jumlahPercobaan + " Percobaan.");
+                    lanjut = false;
+                }
+
+                if (lanjut && jumlahPercobaan < maxTebakan) {
+                    System.out.println("Sisa percobaan: " + (maxTebakan - jumlahPercobaan));
+                }
+
+            }
+
+            if (lanjut == true) {
+                System.out.println("Anda sudah memenuhi batas menebak!");
+                System.out.println("Angka rahasianya nya adalah " + angkaAcak);
+                System.out.println("Terimakasih sudah bermain.");
+            }
+
+            System.out.println("masih mau lanjut? (yes/no)");
+            String pilihanLanjut = input.next();
+            if (pilihanLanjut.equalsIgnoreCase("no")) {
+                mainLagi = false;
+            }
+        }
         input.close();
-
     }
 }
